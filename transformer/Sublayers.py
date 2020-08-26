37bcb91843a10f8ff050e972e612c54f270642d9
yonghee12 <suncar1000@gmail.com>
2020-08-27 07:22:26 +0900

    finished multi head attention

--- transformer/Sublayers.py
+++ transformer/Sublayers.py
@@ -4,21 +4,8 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-
-class LayerNorm(nn.Module):
-    def __init__(self, dim, eps=1e-7):
-        super().__init__()
-        self.gamma = nn.Parameter(torch.ones(dim), requires_grad=True)
-        self.beta = nn.Parameter(torch.zeros(dim), requires_grad=True)
-        self.eps = eps
-        self.dim = dim
-
-    def forward(self, x):
-        # Expect x to be N, T, D
-        assert x.shape[-1] == self.dim
-        mean = x.mean(dim=-1, keepdim=True)
-        std = x.std(dim=-1, keepdim=True)
-        return ((x - mean) / std * self.gamma) + self.beta
+from .Initialzers import *
+from .Modules import *
 
 
 class AddNorm(nn.Module):
@@ -44,29 +31,53 @@ class PositionWiseFeedForward(nn.Module):
         return self.addnorm(x0, Fx)
 
 
-class ScaledDotProductAttention(nn.Module):
-    def __init__(self, d_k: int, dropout: float = 0.1):
-        super().__init__()
-        self.sqrt_d_k = sqrt(d_k)
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, Q, K, V):
-        K_t = K.transpose(1, 2)
-        scaled_dot = (Q @ K_t) / self.sqrt_d_k  # (N, T, d_k) * (N, T, d_k) -> (N, T, T)
-        attention_score = F.softmax(scaled_dot, dim=2)  # N, T, T의 dimension 중 마지막 dimension 기준으로 softmax
-        return attention_score @ V
-
-
 class MultiHeadAttention(nn.Module):
     def __init__(self, d_model, n_heads, d_k=None, dropout=0.1):
         super().__init__()
+        assert d_model % n_heads == 0
         self.d_k = d_model // n_heads if d_k is None else d_k
-        self.attentions = nn.Module([ScaledDotProductAttention(self.d_k, dropout) for _ in range(n_heads)])
+        self.d_model = d_model
+        self.n_heads = n_heads
+
+        self.Wq = nn.Parameter(he(d_model, self.d_k * n_heads), requires_grad=True)
+        self.Wk = nn.Parameter(he(d_model, self.d_k * n_heads), requires_grad=True)
+        self.Wv = nn.Parameter(he(d_model, self.d_k * n_heads), requires_grad=True)
+        self.Wo = nn.Parameter(he(self.d_k * n_heads, d_model), requires_grad=True)
+
+        # method 1 사용시
+        self.attentions = nn.ModuleList([ScaledDotProductAttention(self.d_k, dropout) for _ in range(n_heads)])
+        # method 2 사용시
+        self.attention = ScaledDotProductAttention(self.d_k, dropout)
 
     def forward(self, q, k, v):
-        Q = q @ Wq
-        K = k @ Wk
-        V = v @ Wv
+        # N: n_batch, T: seq_len
+        # q, k, v는 (N, T, d_model), Wq, Wk, Wv는 (d_model, d_k)
+        # Q, K, V는 N, T, d_k * n_heads (논문대로 학습하면 d_k * n_heads는 d_model과 같도록)
 
-        multiheads = np.hstack((head1, head2, head3, head4, head5, head6, head7, head8))  # shape: (s, 64 * 8)
-        output = multiheads @ W  # shape: (s, 512) @ (512, 512) -> (s, 512)
+        Q = q @ self.Wq
+        K = k @ self.Wk
+        V = v @ self.Wv
+
+        n_batch, seq_len, n_heads, d_k = Q.shape[0], Q.shape[1], self.n_heads, self.d_k
+
+        # 1. 논문 그대로 잘라서 각각 attention한 후 합치는 방법
+        # (N, T, d_k * n_heads)를 n_heads로 chunk 나누어 하나의 head: (N, T, d_k)를 만듦
+
+        # q_heads = torch.chunk(Q, chunks=self.n_heads, dim=2)                            # n_heads, N, T, d_k
+        # k_heads = torch.chunk(K, chunks=self.n_heads, dim=2)                            # n_heads, N, T, d_k
+        # v_heads = torch.chunk(V, chunks=self.n_heads, dim=2)                            # n_heads, N, T, d_k
+        # heads_output = [attention(qh, kh, vh) for qh, kh, vh, attention in              # n_heads, N, T, d_k
+        #                 zip(q_heads, k_heads, v_heads, self.attentions)]
+        # heads_concat = torch.cat(heads_output, dim=2)                                   # N, T, d_model
+        # output = heads_concat
+
+        # 2. Vectorized Method
+        # 목적: N, n_heads, T, d_k 모양을 만들어서 attention 진행
+        Q_ = torch.transpose(Q.view(n_batch, seq_len, self.n_heads, self.d_k), 1, 2)   # N, n_heads, T, d_k
+        K_ = torch.transpose(K.view(n_batch, seq_len, self.n_heads, self.d_k), 1, 2)   # N, n_heads, T, d_k
+        V_ = torch.transpose(V.view(n_batch, seq_len, self.n_heads, self.d_k), 1, 2)   # N, n_heads, T, d_k
+        output = self.attention(Q_, K_, V_)                                            # N, n_heads, T, d_k
+        output = output.transpose(1, 2)                                                # N, T, n_heads, d_k
+        output = output.contiguous().view(n_batch, seq_len, self.n_heads * self.d_k)   # N, T, n_heads * self.d_k(=d_model)
+
+        return output @ self.Wo

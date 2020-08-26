a94eb004e7d80947506fab0df54019f5d67a0f7c
yonghee12 <suncar1000@gmail.com>
2020-08-26 17:39:30 +0900

    added Encoder and Decoder blocks

--- transformer/Blocks.py
+++ transformer/Blocks.py
@@ -1,6 +1,29 @@
-class EncoderBlock:
-    pass
+from math import sqrt
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+from .Layers import *
+
+
+class EncoderBlock(nn.Module):
+    def __init__(self, n_layers):
+        super().__init__()
+        self.layers = nn.ModuleList([EncoderLayer() for _ in range(n_layers)])
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
 
 
 class DecoderBlock:
-    pass
+    def __init__(self, n_layers):
+        super().__init__()
+        self.layers = nn.ModuleList([DecoderLayer() for _ in range(n_layers)])
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x

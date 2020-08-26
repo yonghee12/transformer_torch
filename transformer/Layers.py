7a85838dd581038c978e90765e1cfa7aa974e837
yonghee12 <suncar1000@gmail.com>
2020-08-26 17:20:39 +0900

    added decoderlayer

--- transformer/Layers.py
+++ transformer/Layers.py
@@ -1,18 +1,35 @@
-from Sublayers import *
+from .Sublayers import *
+
 
 class EncoderLayer(nn.Module):
     def __init__(self):
         super().__init__()
-        self.multi_head_attention = MultiHeadAttention()
+        self.self_attention = MultiHeadAttention()
         self.feed_forward = PositionWiseFeedForward()
 
-    def forward(self, x0):
-        x1 = self.multi_head_attention(x0)
+    def forward(self, enc_input):
+        x1 = self.self_attention(enc_input)
         x2 = self.feed_forward(x1)
+        return x2
 
 
 class DecoderLayer:
-    pass
+    def __init__(self):
+        super().__init__()
+        self.self_attention = MultiHeadAttention()
+        self.encoder_attention = MultiHeadAttention()
+        self.feed_forward = PositionWiseFeedForward()
+
+    def forward(self, x0, enc_out, ):
+        x1 = self.self_attention(x0, x0, x0)
+        x2 = self.encoder_attention(x1, enc_out, enc_out)
+        x3 = self.feed_forward(x2)
+        return x3
+
 
 class InputLayer:
+    pass
+
+
+class OutputLayer:
     pass

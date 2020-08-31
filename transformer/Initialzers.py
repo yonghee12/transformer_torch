f1a21ba20614d54e96713ccfa6009612fec68f40
yonghee12 <suncar1000@gmail.com>
2020-09-01 01:46:12 +0900

    added randint initializers

--- transformer/Initialzers.py
+++ transformer/Initialzers.py
@@ -2,7 +2,7 @@ import torch
 from torch.nn.init import *
 
 
-def xavier(*shape, requires_grad=False):
+def xavier(*shape, requires_grad=True):
     w = torch.empty(*shape, requires_grad=requires_grad)
     return xavier_normal_(w)
 
@@ -10,3 +10,11 @@ def xavier(*shape, requires_grad=False):
 def he(*shape, requires_grad=True):
     w = torch.empty(*shape, requires_grad=requires_grad)
     return kaiming_normal_(w, mode='fan_out', nonlinearity='relu')
+
+
+def randint10(*shape, requires_grad=False):
+    return torch.randint(low=0, high=10, size=(*shape,), requires_grad=requires_grad)
+
+
+def randint100(*shape, requires_grad=False):
+    return torch.randint(low=0, high=100, size=(*shape,), requires_grad=requires_grad)
